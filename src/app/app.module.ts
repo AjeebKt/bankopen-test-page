0736de956da7b8edde732cdd050381020797e6e2
Ajeeb <ajeebajb@gmail.com>
2021-01-20 20:21:07 +0530

    calculator form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FeaturesComponent } from './features/features.component';
 import { CardComponent } from './elements/card/card.component';
 import { ButtonComponent } from './elements/button/button.component';
 import { EligibilityCalculatorComponent } from './elements/eligibility-calculator/eligibility-calculator.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { EligibilityCalculatorComponent } from './elements/eligibility-calculato
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/elements/eligibility-calculator/eligibility-calculator.component.ts
+++ src/app/elements/eligibility-calculator/eligibility-calculator.component.ts
@@ -1,3 +1,4 @@
+import { getLocaleFirstDayOfWeek } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eligibility-calculator.component.scss']
 })
 export class EligibilityCalculatorComponent implements OnInit {
-
+  incomeMin: number = 100000;
+  incomeMax: number = 300000;
+  monthlyIncome: number = 0;
+  monthlyExpence: number = 0;
+  expenceMin: number = 10000;
+  expenceMax: number = 100000;
+  incomePercent: number = 0;
+  expencePercent: number;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  monthlyIncomeChange(e) {
+
+    this.incomePercent = (100 * e) / (this.incomeMin + this.incomeMax);
+    // console.log((100 * e) / (this.incomeMin + this.incomeMax));
+
+  }
+
+  monthlyExpenceChange(e) {
+
+    this.expencePercent = (100 * e) / (this.expenceMin + this.expenceMax);
+    // console.log((100 * e) / (this.incomeMin + this.incomeMax));
+
+  }
 }
